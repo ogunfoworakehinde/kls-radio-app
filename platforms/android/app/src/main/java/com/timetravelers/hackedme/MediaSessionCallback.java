9c028f95c66fa1f7f23b5d0a6f49795e6f1f96a0
ogunfoworakehinde <ogunfoworakehinde55@gmail.com>
2026-01-24 13:58:37 +0100

    Fix MediaSessionCallback superclass and override errors

--- platforms/android/app/src/main/java/com/timetravelers/hackedme/MediaSessionCallback.java
+++ platforms/android/app/src/main/java/com/timetravelers/hackedme/MediaSessionCallback.java
@@ -1,47 +1,61 @@
 package com.timetravelers.hackedme;
 
 import android.content.Intent;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
+import android.support.v4.media.RatingCompat;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.media.session.MediaButtonReceiver;
-import androidx.media.session.MediaSessionCompat;
-import androidx.media.session.PlaybackStateCompat;
-import androidx.media.session.MediaSessionCompat.Callback;
+import android.util.Log;
 
 public class MediaSessionCallback extends MediaSessionCompat.Callback {
 
+    private static final String TAG = "MediaSessionCallback";
+
     @Override
     public void onPlay() {
-        super.onPlay();
-        // TODO: call your background audio play function
+        Log.d(TAG, "onPlay called");
+        // Call your Cordova plugin method to resume playback
+        BackgroundAudioBridge.play();
     }
 
     @Override
     public void onPause() {
-        super.onPause();
-        // TODO: call your background audio pause function
+        Log.d(TAG, "onPause called");
+        // Call your Cordova plugin method to pause playback
+        BackgroundAudioBridge.pause();
     }
 
     @Override
-    public void onSkipToNext() {
-        super.onSkipToNext();
-        // TODO: implement skip to next if you have multiple streams
-    }
-
-    @Override
-    public void onSkipToPrevious() {
-        super.onSkipToPrevious();
-        // TODO: implement skip to previous if you have multiple streams
+    public void onStop() {
+        Log.d(TAG, "onStop called");
+        // Call your Cordova plugin method to stop playback
+        BackgroundAudioBridge.stop();
     }
 
     @Override
     public void onSeekTo(long pos) {
-        super.onSeekTo(pos);
-        // TODO: implement seek for your stream
+        Log.d(TAG, "onSeekTo called: " + pos);
+        // Optionally implement if your stream supports seeking
     }
 
     @Override
-    public boolean onMediaButtonEvent(@NonNull Intent mediaButtonIntent) {
+    public void onSkipToNext() {
+        Log.d(TAG, "onSkipToNext called");
+        // Optionally implement if you have multiple streams
+        BackgroundAudioBridge.skipNext();
+    }
+
+    @Override
+    public void onSkipToPrevious() {
+        Log.d(TAG, "onSkipToPrevious called");
+        // Optionally implement if you have multiple streams
+        BackgroundAudioBridge.skipPrevious();
+    }
+
+    @Override
+    public boolean onMediaButtonEvent(Intent mediaButtonIntent) {
+        Log.d(TAG, "onMediaButtonEvent called");
+        // Let the default handler process the media button
         return super.onMediaButtonEvent(mediaButtonIntent);
     }
 }
