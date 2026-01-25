847aa3a92aa3a89340981943a282128b50c5f1a9
Jayeoba O.G Kehinde <106041710+ogunfoworakehinde@users.noreply.github.com>
2026-01-25 15:25:20 -0800

    Refactor radio streams into KLSRadioStreams object
    Refactored radio stream definitions into an object with methods for accessing station data.

--- www/js/streams.js
+++ www/js/streams.js
@@ -1,18 +1,53 @@
-// Define all your radio streams here
-const STREAMS = [
-    {
-        name: "English Gospel",
-        url: "https://s3.voscast.com:9425/stream",
-        description: "24/7 English Gospel Music"
+/**
+ * Stream URLs for KLS Radio
+ * You can modify these URLs as needed
+ */
+
+const KLSRadioStreams = {
+    stations: [
+        {
+            id: 'english',
+            name: 'English Gospel',
+            url: 'https://s3.voscast.com:9425/stream',
+            description: '24/7 English Gospel Music',
+            genre: 'Gospel'
+        },
+        {
+            id: 'yoruba',
+            name: 'Yoruba Gospel',
+            url: 'https://s3.voscast.com:10745/stream',
+            description: 'Yoruba Language Worship',
+            genre: 'Gospel'
+        },
+        {
+            id: 'praise',
+            name: 'Praise Worship',
+            url: 'https://stream.zeno.fm/f3wvbbqmdg8uv',
+            description: 'Contemporary Praise',
+            genre: 'Worship'
+        }
+    ],
+    
+    // Get station by ID
+    getStation: function(id) {
+        return this.stations.find(station => station.id === id);
     },
-    {
-        name: "Yoruba Gospel",
-        url: "https://s3.voscast.com:10745/stream",
-        description: "Yoruba Language Worship"
+    
+    // Get station by index
+    getStationByIndex: function(index) {
+        return this.stations[index];
     },
-    {
-        name: "Praise Worship",
-        url: "https://stream.zeno.fm/f3wvbbqmdg8uv",
-        description: "Contemporary Praise"
+    
+    // Get all stations
+    getAllStations: function() {
+        return this.stations;
+    },
+    
+    // Get total number of stations
+    getStationCount: function() {
+        return this.stations.length;
     }
-];
+};
+
+// Export as global
+window.KLSRadioStreams = KLSRadioStreams;
